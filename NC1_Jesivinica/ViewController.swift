b109ef30204d6289c10da60f39d22330c1dbef16
Jesivinica Santoso <jesivinica@gmail.com>
2022-04-26 09:17:12 +0700

    Initial Commit

---
+++ NC1_Jesivinica/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NC1_Jesivinica
+//
+//  Created by Jesivinica Santoso on 26/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
